579dd64d91dfc99e9b4e35846d2cd13d6e9b069d
NPA Sherly Anggita Sugiarta <50704279+nptayusherly@users.noreply.github.com>
2022-04-01 21:58:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -113,108 +113,15 @@ decimal.addEventListener("click", (event) => {
     updateScreen(currentNumber);
 });
 
+const percentageNumber = (() => {
+    if (currentNumber === '0') {
+        return;
+    }
+    currentNumber = currentNumber / 100;
+});
 
-
-
-
-
-
-
-// const calculatorScreen = document.querySelector('.calculator-screen');
-// const updateScreen = (number) => {
-//     calculatorScreen.value = number;
-// }
-
-
-// let prevNumber = '';
-// let calculationOperator = '';
-// let currentNumber = '0';
-
-// const inputNumber = ((number) => {
-//     if(currentNumber === '0') {
-//         currentNumber = number;
-//     } else {
-//         currentNumber += number;
-//     }
-// });
-
-// const numbers = document.querySelectorAll(".number");
-// numbers.forEach(number => {
-//     number.addEventListener("click", (event) => {
-//         console.log(event.target.value);
-//         inputNumber(event.target.value);
-//         updateScreen(currentNumber);
-//     });
-// });
-
-// const inputOperator = ((operator) => {
-//     if(calculationOperator === '') {
-//         prevNumber = currentNumber;
-//     }
-//     calculationOperator = operator;
-//     currentNumber = '0';
-// });
-
-// const operators = document.querySelectorAll(".operator");
-// operators.forEach(operator => {
-//     operator.addEventListener("click", (event) => {
-//         console.log(event.target.value);
-//         inputOperator(event.target.value);
-//         updateScreen(calculationOperator);
-//     });
-// });
-
-// const calculate = () => {
-//     let result = '';
-//     switch(calculationOperator) {
-//         case "+":
-//             result = parseFloat(prevNumber) + parseFloat(currentNumber);
-//             // result = result.toPrecision(12);
-//             break;
-//         case "-":
-//             result = parseFloat(prevNumber) - parseFloat(currentNumber);
-//             break;
-//         case "*":
-//             result = parseFloat(prevNumber) * parseFloat(currentNumber);
-//             break;
-//         case "/":
-//             result = parseFloat(prevNumber) / parseFloat(currentNumber);
-//             break;
-//         default:
-//             break;
-//     }
-//     currentNumber = result;
-//     calculationOperator = '';
-// }
-
-// const equalSign = document.querySelector(".equal-sign");
-// equalSign.addEventListener("click", () => {
-//     calculate();
-//     updateScreen(currentNumber);
-// });
-
-// const cleasAll = () => {
-//     prevNumber = '';
-//     calculationOperator = '';
-//     currentNumber = '0';
-// }
-
-// const clearBtn = document.querySelector(".all-clear");
-// clearBtn.addEventListener("click", (event) => {
-//     console.log(event.target.value);
-//     cleasAll();
-//     updateScreen(currentNumber);
-// });
-
-// const inputDecimal = (dot) => {
-//     if(currentNumber.includes(".")) {
-//         return
-//     } 
-//     currentNumber += dot;
-// }
-
-// const decimal = document.querySelector(".decimal");
-// decimal.addEventListener("click", (event) => {
-//     inputDecimal(event.target.value);
-//     updateScreen(currentNumber);
-// });
+const percentage = document.querySelector(".percentage");
+percentage.addEventListener("click", (event) => {
+    percentageNumber(event.target.value);
+    updateScreen(currentNumber);
+});
